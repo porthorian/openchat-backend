38f69111793f38f3ff563494c7ddbf5549f8d479
Vinnie Marone <vinnie.marone35@gmail.com>
2026-02-17 15:45:22 -0500

    fix: cors issue when sending user avatar

--- internal/api/middleware_cors.go
+++ internal/api/middleware_cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-OpenChat-User-UID, X-OpenChat-Device-ID")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, If-Match, X-OpenChat-User-UID, X-OpenChat-Device-ID")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
